6acb91d52c74263c992aee532876cb60172a5855
yaoyao <yao.yao@yiducloud.com>
2019-11-28 16:25:25 +0800

    yy

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import Index from "./views/index/index";
+import List from "./views/list/index";
 
 const BasicRoute = () => (
     <HashRouter>
         <Switch>
             <Route exact path="/" component={Index}/>
+            <Route exact path="/list" component={List}/>
         </Switch>
     </HashRouter>
 );
---
+++ src/views/list/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Index() {
+  return (
+    <div className="list-wrap">
+      321      
+    </div>
+  );
+}
+
+export default Index;
