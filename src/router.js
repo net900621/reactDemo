4562a790a02253e1e8b81415bbe6a6e383a79426
yaoyao <yao.yao@yiducloud.com>
2019-11-28 17:31:48 +0800

    yy

--- src/router.js
+++ src/router.js
@@ -3,11 +3,32 @@ import {HashRouter, Route, Switch} from 'react-router-dom';
 import Index from "./views/index/index";
 import List from "./views/list/index";
 
+const setTitle = (title) => {
+    document.title = title
+};
+
+export const routes = [{
+    path: '/',
+    title: '首页',
+    component: Index
+},{
+    path: '/list',
+    title: '影评列表',
+    component: List
+}]
+
 const BasicRoute = () => (
     <HashRouter>
         <Switch>
-            <Route exact path="/" component={Index}/>
-            <Route exact path="/list" component={List}/>
+            {routes.map(({ path, title, component }, index) => (
+                <Route
+                    key={index}
+                    exact
+                    path={path}
+                    onEnter={() => setTitle(title)}
+                    component={component}
+                />
+            ))}
         </Switch>
     </HashRouter>
 );
